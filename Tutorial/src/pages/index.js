a0e445b98d38e78f377518da73569986031f74da
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-01 19:56:06 -0500

    css modules

--- Tutorial/src/pages/index.js
+++ Tutorial/src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
 import Layout from "../../components/Layout"
-import "../../components/layout.css"
+// import "../../components/layout.css"
+import { page, text } from "../../examples/home.module.css"
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className={page}>
+        <h1>Home Page</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          tempora a perspiciatis aut eaque tenetur ducimus cumque culpa ea
+          expedita.
+        </p>
+      </div>
     </Layout>
   )
 }
