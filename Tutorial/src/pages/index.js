e3d2e3df34f5bc96ab247ac66052b8712afe6eb5
karimmoustamid <moustamid.tech@gmail.com>
2023-01-28 16:40:30 -0600

    Css Modules

--- Tutorial/src/pages/index.js
+++ Tutorial/src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
 import Layout from "../components/Layout";
-
+import {header} from '../../exemples/home.module.css'
 export default function Home() {
     return <Layout>
-        <h1>Hello Karim !</h1>
+        <h1 className={header}>Hello Karim !</h1>
     </Layout>
 }
